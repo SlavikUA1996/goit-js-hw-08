dbffc7424879c0e5d9fcad2b05204b7c4e9287c3
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-04-10 15:11:17 +0300

    first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryCreate = document.querySelector('.gallery');
+
+const galleryEl = galleryItems.map(({ preview, description, original }) =>
+    `<div class = "gallery__item">
+<a class = "gallery__link" href = "${original}">
+<img class = "gallery__image" src ="${preview}" data-source="${original}" alt = "${description}" />
+</a>
+</div>`
+).join('');
+
+galleryCreate.insertAdjacentHTML('beforeend', galleryEl);
+
+const lightboxCreate = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, captionPosition: "bottom", });
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+
+
+
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+const TIME_KEY = 'current-time-video';    
+
+timeToSet();
+
+function timeUpdate({second}) {
+    localStorage.setItem(TIME_KEY, second);
+    
+    if (currentTime) {
+        player.setCurrentTime(currentTime); 
+    }
+}
+player.on('timeupdate', throttle(timeUpdate, 1000))
